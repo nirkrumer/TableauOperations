120595b943e3896e993072752523eb90a544fbc3
nirkrumer <nir.krumer@naturalint.com>
2020-05-11 11:57:01 +0300

    initial commit

---
+++ venv/MediaProfit.py
@@ -0,0 +1,69 @@
+import tableauserverclient as TSC
+#import ObjectGetters ;
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from datetime import date
+import datetime
+import os
+
+    # ----------------------- UTILS ---------------------------------
+
+def getWorkbookByName(server, wb_name):
+    workbooks = [x for x in TSC.Pager(server.workbooks) if x.name == wb_name]
+    return None if len(workbooks) == 0 else workbooks.pop()
+
+def getUserListByGroup(server, group_name):
+    groups = [x for x in TSC.Pager(server.groups) if x.name == group_name]
+    return None if len(groups) == 0 else groups.pop()
+
+def getViewByWbId(server, wbId, viewName):
+     views = [x for x in TSC.Pager(server.views) if (x.workbook_id == wbId and x.name == viewName)]
+     return None if len(views) == 0 else views.pop()
+
+# TableauAdminPass = os.environ.get("TableauAdminPass")
+# tableau_auth = TSC.TableauAuth('admin', TableauAdminPass)
+tableau_auth = TSC.TableauAuth('admin', 'xqKE4ynYHzoGCiVwPWsBGZrT')
+server = TSC.Server('https://tableau.naturalint.com',use_server_version=True)
+
+with server.auth.sign_in(tableau_auth):
+    view_item = (getViewByWbId(server,
+            getWorkbookByName(server, "Media Profit").id,"Management Dashboard"))
+    group = (getUserListByGroup(server,"Management V2.0"))
+    pagination_item = server.groups.populate_users(group)
+    # for user in group.users:
+    #     print(user.name)
+
+    server.views.populate_image(view_item)
+    with open('./Screenshots/dashboard-screenshot-'+ str(date.today()) +'.png', 'wb') as f:
+         f.write(view_item.image)
+    server.auth.sign_out()
+# ----------------- EMAIL SETTINGS -------------------------
+
+    port = 465
+    context = ssl.create_default_context()
+    sender_email = "boomi@naturalint.com"
+    receiver_email = "nir.krumer@naturalint.com"
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Management Dashboard - " + str(date.today())
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    messageText = '<html><body><h3>dear all, please review ' + str(datetime.datetime.now().strftime("%B")) + \
+                  ' Rev & GP status as of ' + str(date.today()) + ' below & linked' \
+                  '</h3>' + '<p>' '<img src="cid:image1" height="800" width="1200">' \
+                    '</p>' + '</body></html>'
+    message.attach(MIMEText(messageText, "html"))
+
+    fp = open('./Screenshots/dashboard-screenshot-'+ str(date.today()) +'.png', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+
+    msgImage.add_header('Content-ID', '<image1>')
+    message.attach(msgImage)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("boomi@naturalint.com", "b89qPL4b")
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+        server.quit()
---
+++ venv/ObjectGetters.py
@@ -0,0 +1,63 @@
+import tableauserverclient as TSC
+
+
+# Server #
+
+def tableau_sign_in(url_server, user_name, password):
+    tableau_auth = TSC.TableauAuth(user_name, password)
+    server = TSC.Server(url_server)
+    server.version = os.getenv("TABLEAU_API_VERSION", default="3.2")
+    server.auth.sign_in(tableau_auth)
+    return server
+
+
+def tableauSignOut(server):
+    server.auth.sign_out()   
+
+# ------------------------------------------------------------------
+
+def getWorkbookByName(server, wb_name):
+    workbooks = [x for x in TSC.Pager(server.workbooks) if x.name == wb_name]
+    return None if len(workbooks) == 0 else workbooks.pop()
+
+def getUserListByGroup (server, group_name):
+
+    groups = [x for x in TSC.Pager(server.groups) if x.name == group_name]
+    return None if len(groups) == 0 else groups.pop()
+
+def getViewByWbId(server, wbId, viewName):
+    views = [x for x in TSC.Pager(server.views)
+             if (x.workbook_id == wbId and x.name == viewName)]
+    # views = []
+    # for x in TSC.Pager(server.views):
+    #     if (x.workbook_id == wbId and x.name == viewName):
+    #         views.append(x)
+    return None if len(views) == 0 else views.pop()
+
+def getSubsciptionByName(server, wb_name):
+    req_option = TSC.RequestOptions()
+    req_option.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, wb_name))
+    matching_workbooks, pagination_item = server.subscriptions.get(req_option)
+    assert len(matching_workbooks) > 0, 'No subscription named {0} found'.format(wb_name)
+    workbook = matching_workbooks[0]
+    return workbook
+
+def getUserByEmail(server, email):
+    req_option = TSC.RequestOptions()
+    req_option.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, email))
+    matching_users, pagination_item = server.users.get(req_option)
+    assert len(matching_users) > 0, 'No users named {0} found'.format(email)
+    user = matching_users[0]
+    return user
+
+def getUserById(server, id):
+    req_option = TSC.RequestOptions()
+    req_option.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, id))
+    matching_users, pagination_item = server.users.get(req_option)
+    assert len(matching_users) > 0, 'No users named {0} found'.format(id)
+    user = matching_users[0]
+    return user
+
+def getScheduleByName(server, name):
+    schedules = [x for x in TSC.Pager(server.schedules) if x.name == name]
+    return None if len(schedules) == 0 else schedules.pop()
