2cf399631c122705917487b19cffb8e8daa6ba3f
nirkrumer <nir.krumer@naturalint.com>
2020-07-29 15:52:19 +0300

    'refresh'

--- venv/RefreshWorkbook.py
+++ venv/RefreshWorkbook.py
@@ -5,11 +5,9 @@ def getWorkbookByName(server, wb_name):
     workbooks = [x for x in TSC.Pager(server.workbooks) if x.name == wb_name]
     return None if len(workbooks) == 0 else workbooks.pop()
 
-#wbName = os.getenv("wbName");
-wbName = "Workbook Management"
-# TableauAdminPass = os.environ.get("TableauAdminPass")
-# tableau_auth = TSC.TableauAuth('admin', TableauAdminPass)
-tableau_auth = TSC.TableauAuth('admin', 'xqKE4ynYHzoGCiVwPWsBGZrT')
+wbName = os.getenv("wbName");
+TableauAdminPass = os.environ.get("TableauAdminPass")
+tableau_auth = TSC.TableauAuth('admin', TableauAdminPass)
 server = TSC.Server('https://tableau.naturalint.com',use_server_version=True)
 
 with server.auth.sign_in(tableau_auth):
