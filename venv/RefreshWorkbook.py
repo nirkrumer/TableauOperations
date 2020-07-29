1187bdf782cd00361e7898b83a916d4e4f292052
nirkrumer <nir.krumer@naturalint.com>
2020-07-29 17:15:18 +0300

    'ref'

--- venv/RefreshWorkbook.py
+++ venv/RefreshWorkbook.py
@@ -5,6 +5,7 @@ def getWorkbookByName(server, wb_name):
     workbooks = [x for x in TSC.Pager(server.workbooks) if x.name == wb_name]
     return None if len(workbooks) == 0 else workbooks.pop()
 
+flag = False
 wbName = os.getenv("wbName");
 TableauAdminPass = os.environ.get("TableauAdminPass")
 tableau_auth = TSC.TableauAuth('admin', TableauAdminPass)
@@ -12,12 +13,17 @@ server = TSC.Server('https://tableau.naturalint.com',use_server_version=True)
 
 with server.auth.sign_in(tableau_auth):
     all_tasks, pagination_item = server.tasks.get()
+    #wb = getWorkbookByName(server,wbName)
     wb = getWorkbookByName(server,wbName)
 
     for task in all_tasks:
         if task.target.id == wb.id:
-            print("task id = " + task.target.id + ", task sched = "+ task.schedule_id + ",type = " + task.target.type)
+            print("wbName = " + wb.name " runs by schedulue: task id = " + task.target.id + ", task sched = "+ task.schedule_id + ",type = " + task.target.type)
             server.tasks.run(task)
+            flag = True
             break
+    if (flag == False):
+        print("wbName = " + wb.name + "without schedule runs!")
+        results = server.workbooks.refresh(wb.id)
 
 server.auth.sign_out()
